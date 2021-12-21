bcccf21f5ba17c96ce933ed17550c09057c22451
AnakSarawak <96113022+AnakSarawak@users.noreply.github.com>
2021-12-21 23:25:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdrivermanager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
 
